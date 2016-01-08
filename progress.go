07c6aac2adc03c0d78a6cd1c81161d559c75f613
Roland Illig <roland.illig@gmx.de>
2016-01-08 21:58:37 +0000

    ProgressPercentage computation

---
+++ progress.go
@@ -0,0 +1,29 @@
+package misc
+
+// ProgressPercentage calculates the percentage that should be shown in
+// a progress bar when k of n work items are done.
+// A return value of 0 percent means that nothing has been done.
+// 1 means that something has been done, but less than 2 %.
+// (This is the only case in which the displayed progress is higher than
+// the actual progress. It is different from 0 though, to show that
+// something has already been done.) All other values are rounded down,
+// therefore 100 means that everything has been done.
+func ProgressPercentage(k, n int) int {
+	var perc int
+	switch {
+	case k <= 0 || n <= 0:
+		perc = 0
+	case k >= n:
+		perc = 100
+	default:
+		if k100 := 100 * k; k100/100 == k {
+			perc = k100 / n
+		} else {
+			perc = k / (n / 100)
+		}
+		if perc == 0 {
+			perc = 1
+		}
+	}
+	return perc
+}
---
+++ progress_test.go
@@ -0,0 +1,49 @@
+package misc
+
+import "testing"
+
+func TestProgressPercentage(t *testing.T) {
+	var n int
+
+	check := func(k, percentage int) {
+		actual := ProgressPercentage(k, n)
+		if actual != percentage {
+			t.Errorf("Progress %d/%d should be %d %%, is %d %%.", k, n, percentage, actual)
+		}
+	}
+
+	n = 7
+
+	check(-100, 0)
+	check(0, 0)
+	check(1, 14)
+	check(2, 28)
+	check(3, 42)
+	check(4, 57)
+	check(5, 71)
+	check(6, 85)
+	check(7, 100)
+	check(8, 100)
+
+	n = 1000
+
+	check(0, 0)
+	check(1, 1)
+	check(10, 1)
+	check(19, 1)
+	check(20, 2)
+	check(29, 2)
+	check(30, 3)
+	check(623, 62)
+	check(984, 98)
+	check(985, 98)
+	check(999, 99)
+	check(1000, 100)
+
+	n = int(^uint(0) >> 1)
+
+	check(0, 0)
+	check(n/3, 33)
+	check(n/2, 50)
+	check(n, 100)
+}
