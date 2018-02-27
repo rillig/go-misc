f3f15280dfd3dd24fab4f1483a5459ebb8ea71db
Roland Illig <roland.illig@gmx.de>
2018-02-27 16:00:37 +0100

    gctrace-align: remove regular expression from the equation

--- cmd/gctrace-align/gctrace-align.go
+++ cmd/gctrace-align/gctrace-align.go
@@ -1,4 +1,8 @@
 // gctrace-align reads a Go GC log from the given file and aligns the columns.
+//
+// To produce the GC log, set GODEBUG=gctrace=1.
+//
+// See https://github.com/chewiebug/GCViewer/pull/196
 package main
 
 import (
@@ -11,26 +15,11 @@ import (
 	"strings"
 )
 
-var gcLine = regexp.MustCompile("" +
-	"(gc )(\\d+)" +
-	"( @)(\\d+\\.\\d+)" +
-	"(s )(\\d+)" +
-	"(%: )(\\d+(?:\\.\\d+)?)" +
-	"(\\+)(\\d+(?:\\.\\d+)?)" +
-	"(\\+)(\\d+(?:\\.\\d+)?)" +
-	"( ms clock, )(\\d+(?:\\.\\d+)?)" +
-	"(\\+)(\\d+(?:\\.\\d+)?)" +
-	"(/)(\\d+(?:\\.\\d+)?)" +
-	"(/)(\\d+(?:\\.\\d+)?)" +
-	"(\\+)(\\d+(?:\\.\\d+)?)" +
-	"( ms cpu, )(\\d+)" +
-	"(->)(\\d+)" +
-	"(->)(\\d+)" +
-	"( MB, )(\\d+)" +
-	"( MB goal, )(\\d+)" +
-	"( P)")
-
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("usage: %s <gclogfile>", os.Args[0])
+	}
+
 	input, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -44,41 +33,51 @@ func main() {
 }
 
 type Line struct {
-	gc     bool
-	fields []interface{}
+	relevant bool
+	fields   []interface{}
 }
 
 func parse(in []string) []Line {
 	var lines []Line
 	for _, line := range in {
-		match := gcLine.FindStringSubmatch(line)
-		if match != nil {
-			var cells []interface{}
-			for _, field := range match[1:] {
-				var val interface{}
-				var err error
-				val, err = strconv.Atoi(field)
-				if err != nil {
-					val, err = strconv.ParseFloat(field, 64)
-					if err != nil {
-						val = field
-					}
+		if !strings.HasPrefix(line, "gc ") || !strings.HasSuffix(line, " P") {
+			lines = append(lines, Line{false, []interface{}{line}})
+			continue
+		}
+
+		matches := regexp.MustCompile(`(.*?)(\d+(?:\.\d+)?)`).FindAllStringSubmatchIndex(line, -1)
+		if matches == nil {
+			lines = append(lines, Line{false, []interface{}{line}})
+			continue
+		}
+
+		var cells []interface{}
+		for _, match := range matches {
+			for i := 2; i < len(match); i += 2 {
+				field := line[match[i]:match[i+1]]
+				floatVal, err := strconv.ParseFloat(field, 64)
+
+				var val interface{} = field
+				switch {
+				case err != nil:
+					val = field
+				case floatVal == float64(int(floatVal)):
+					val = int(floatVal)
+				default:
+					val = floatVal
 				}
+
 				cells = append(cells, val)
 			}
-			lines = append(lines, Line{true, cells})
-		} else {
-			lines = append(lines, Line{false, []interface{}{line}})
 		}
+
+		if lastMatch := matches[len(matches)-1]; lastMatch[1] != len(line) {
+			cells = append(cells, line[lastMatch[1]:])
+		}
+
+		lines = append(lines, Line{true, cells})
 	}
 	return lines
-}
-
-func intMax(a int, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 func formatTable(table []Line) []string {
@@ -87,22 +86,23 @@ func formatTable(table []Line) []string {
 		cols = intMax(cols, len(row.fields))
 	}
 
-	maxWidth := make([]int, cols, cols)
-	maxBefore := make([]int, cols, cols)
-	maxAfter := make([]int, cols, cols)
+	maxWidth := make([]int, cols, cols)  // Total width of the field
+	maxBefore := make([]int, cols, cols) // Number of characters before the point
+	maxAfter := make([]int, cols, cols)  // Number of characters after the point
 
 	for _, line := range table {
-		if line.gc {
+		if line.relevant {
 			fields := line.fields
 			for i, field := range fields {
 				str := fmt.Sprintf("%v", field)
-				if _, ok := field.(int); ok {
+				switch field.(type) {
+				case int:
 					maxBefore[i] = intMax(maxBefore[i], len(str))
-				} else if _, ok := field.(float64); ok {
+				case float64:
 					point := strings.IndexByte(str, '.')
 					maxBefore[i] = intMax(maxBefore[i], point)
 					maxAfter[i] = intMax(maxAfter[i], len(str)-(point+1))
-				} else {
+				case string:
 					maxWidth[i] = intMax(maxWidth[i], len(str))
 				}
 			}
@@ -118,8 +118,9 @@ func formatTable(table []Line) []string {
 	}
 
 	var result []string
-	for _, row := range table {
-		if !row.gc {
+	for rowno, row := range table {
+		_ = rowno
+		if !row.relevant {
 			result = append(result, row.fields[0].(string))
 			continue
 		}
@@ -128,19 +129,31 @@ func formatTable(table []Line) []string {
 		fields := row.fields
 		for i, obj := range fields {
 			str := fmt.Sprintf("%v", obj)
+
+			leftPad := 0
 			switch obj.(type) {
 			case int:
-				str = strings.Repeat(" ", maxBefore[i]-len(str)) + str
+				leftPad = maxBefore[i] - len(str)
 			case float64:
-				str = strings.Repeat(" ", maxBefore[i]-strings.IndexByte(str, '.')) + str
-			}
-			if i != len(fields)-1 {
-				str += strings.Repeat(" ", maxWidth[i]-len(str))
+				leftPad = maxBefore[i] - strings.IndexByte(str, '.')
 			}
 
-			resultRow = append(resultRow, str)
+			rightPad := 0
+			if i != len(fields)-1 {
+				rightPad = maxWidth[i] - leftPad - len(str)
+			}
+
+			padded := strings.Repeat(" ", leftPad) + str + strings.Repeat(" ", rightPad)
+			resultRow = append(resultRow, padded)
 		}
 		result = append(result, strings.Join(resultRow, ""))
 	}
 	return result
+}
+
+func intMax(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
